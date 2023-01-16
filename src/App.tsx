0372b7f25c1249e93c3010991b3c138f7846c8a0
Parul <parulagarwal1605@gmail.com>
2023-01-16 16:58:54 +0530

    Logic implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import ProductList from './Products/ProductList';
-import store from './store';
-import './App.css';
-import { Provider } from 'react-redux';
-import ProductForm from './Products/productForm';
-import Cart from './Cart/cart';
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+import { Navbar } from "./components/Navbar"
 
 function App() {
   return (
-    <>
-    <Provider store={store}>
-    <div className="App">
-      <ProductList/>
-      <ProductForm/>
-      <Cart/>
-    </div>
-    </Provider>
-    </>
-  );
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  )
 }
 
-export default App;
+export default App
