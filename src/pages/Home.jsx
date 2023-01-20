f66c1c86f5d7f9dd59557823ec7ab2696b48a318
Parul <parulagarwal1605@gmail.com>
2023-01-20 13:03:44 +0530

    Code modification

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,23 @@
 import Form from "../components/Form";
-//import { Navbar } from "../components/Navbar";
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import EditContact from "../components/EditContact";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-//import { contactReducer } from "./redux/contactReducer";
+import { Routes,Route } from "react-router-dom";
 import { composeWithDevTools } from "redux-devtools-extension";
+import {createStore} from "redux";
+import { Provider } from "react-redux";
 import { contactReducer } from "../redux/contactReducer";
 import { ToastContainer } from "react-toastify";
+import EditContact from "../components/EditContact";
 import AddPost from "../components/AddContact";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+
 
 const store = createStore(contactReducer, composeWithDevTools());
 export function Home() {
   return(<div className="App">
   <ToastContainer />
   
+
   <Provider store={store}>
-  
   <Routes>
   <Route  path="/" element={<Form />} />
   <Route path="/add" element={<AddPost/>} />
